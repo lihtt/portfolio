844796b4efca1722093d58ebe3de3a08e7a08d08
Alex <alexeychamploo@gmail.com>
2024-04-28 13:12:11 +0500

    add up button, smooth animations for interal links and some libs

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,41 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+$(document).ready(function(){
+
+    // Плавная прокрутка для всех ссылок с атрибутом href, начинающимся на #
+    $('a[href^="#"]').click(function(e){
+        // Предотвращаем стандартное поведение браузера
+        e.preventDefault();
+
+        // Получаем id элемента, к которому нужно перейти
+        var id = $(this).attr('href');
+
+        // Получаем расстояние от начала страницы до элемента
+        var targetOffset = $(id).offset().top;
+
+        // Плавно прокручиваем страницу к нужному элементу
+        $('html, body').animate({
+            scrollTop: targetOffset
+        }, 1000); // 1000 миллисекунд на прокрутку
+    });
+
+    // Smooth scroll & pageup
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1000) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
+});
